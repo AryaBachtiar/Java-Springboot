a08eb0e4d7342d76f402b44940c443249ecb60f5
AryaBachtiar <aryabachtiar89@gmail.com>
2022-11-23 15:16:00 +0700

    Toko

--- src/main/java/com/toko/tokoonline/TokoonlineApplication.java
+++ src/main/java/com/toko/tokoonline/TokoonlineApplication.java
@@ -2,8 +2,10 @@ package com.toko.tokoonline;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class TokoonlineApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/toko/tokoonline/model/Barang.java
+++ src/main/java/com/toko/tokoonline/model/Barang.java
@@ -1,10 +1,12 @@
 package com.toko.tokoonline.model;
 
+import com.toko.tokoonline.auditing.Auditable;
+
 import javax.persistence.*;
 
 @Entity
 @Table(name = "barang")
-public class Barang {
+public class Barang extends Auditable {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
